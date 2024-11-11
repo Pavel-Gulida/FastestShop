7b71a6901ed48d5254e0284a53b65411f88ab9e9
unknown <pavel.gulida@gmail.com>
2024-11-11 17:21:34 +0300

    Added HW27

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -17,7 +17,7 @@ DATABASES = {
         "HOST": os.environ.get("POSTGRES_HOST"),
         "PORT": os.environ.get("POSTGRES_PORT"),
     },
-    "default": {
+    "default_sqlite": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",  # NOQA
     }
